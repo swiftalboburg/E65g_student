42a8e4c5c8cdd30952ab414854050be8dd718be6
swiftalboburg <alboburg@gmail.com>
2017-03-27 22:12:19 -0400

    Assignment 3 back to github

--- Assignment3/Assignment3/Grid.swift
+++ Assignment3/Assignment3/Grid.swift
@@ -1,7 +1,8 @@
 //
-//  Grid.swift
+//  Grid.swift Ana Nystedt
 //
 import Foundation
+import UIKit 
 
 public typealias Position = (row: Int, col: Int)
 public typealias PositionSequence = [Position]
@@ -22,17 +23,27 @@ public func positionSequence (from: Position, to: Position) -> PositionSequence 
         .flatMap { $0 }
 }
 
+/*
+I assigned the explicit values, even though it is not necessary since the names are the same -  following the assignment instructions
+ */
 public enum CellState : String {
-    case alive, empty, born, died
+    case alive = "alive"
+    case empty = "empty"
+    case born = "born"
+    case died = "died"
     
+
     public var isAlive: Bool {
         switch self {
         case .alive, .born: return true
         default: return false
         }
     }
-    public func description() -> String {  // do we really need a switch statement if I already have the names as rawvalues?
-        
+    
+/*
+I used the switch statement following the assignment instructions (to get the points ;) I don't see the need in this case, since we are returning the rawValue.  I hope I did not misundersand the instructions.  
+*/
+    public func description() -> String {  
         switch self {
         case .alive:
             return rawValue
@@ -42,15 +53,19 @@ public enum CellState : String {
             return rawValue
         case .died:
             return rawValue
+            
         }
-        
+ 
+ 
     }
     
-    static func allValues() -> [String] {
+    
+    
+    public func allValues() -> [String] {
         return ["alive","empty","born","died"]
     }
     
-    public func toggle(value:CellState)-> CellState {
+    public func toggle(_ value:CellState)-> CellState {
         switch value {
         case .empty,  .died:
             return .alive
@@ -197,4 +212,138 @@ func gliderInitializer(row: Int, col: Int) -> CellState {
     default: return .empty
     }
 }
+
+
+
+@IBDesignable class GridView: UIView {
+    var theGrid = Grid(0,0)
+    
+    
+    @IBInspectable var livingColor : UIColor  = UIColor.blue
+    
+    @IBInspectable var emptyColor : UIColor = UIColor.white
+    
+    @IBInspectable var bornColor : UIColor = UIColor.green
+    
+    @IBInspectable var diedColor : UIColor = UIColor.black
+    
+    @IBInspectable var gridColor : UIColor = UIColor.gray
+    
+    @IBInspectable var gridWidth : CGFloat = 0.0
+
+    @IBInspectable var size : Int = 20 {
+        didSet{
+            theGrid = Grid(size, size)
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let boardSize = CGSize(
+            width: rect.size.width/CGFloat(size),
+            height: rect.size.height/CGFloat(size)
+        )
+        let base = rect.origin
+        (0 ..< size).forEach { i in
+            (0 ..< size).forEach { j in
+                let origin = CGPoint(
+                    x: base.x + (CGFloat(j) * boardSize.width),
+                    y: base.y + (CGFloat(i) * boardSize.height)
+                )
+                let subRect = CGRect(
+                    origin: origin,
+                    size: boardSize
+                )
+                let path = UIBezierPath(ovalIn: subRect)
+               
+                
+                theGrid[Position(i,j)].isAlive ? livingColor.setFill() : emptyColor.setFill()
+                
+                path.fill()
+            }
+        }
+        
+
+        //create the path
+        (0 ..< size+1 ).forEach {
+            drawLine(
+                start: CGPoint(x: CGFloat($0)/CGFloat(size) * rect.size.width, y: 0.0),
+                end:   CGPoint(x: CGFloat($0)/CGFloat(size) * rect.size.width, y: rect.size.height)
+            )
+            
+            drawLine(
+                start: CGPoint(x: 0.0, y: CGFloat($0)/CGFloat(size) * rect.size.height ),
+                end: CGPoint(x: rect.size.width, y: CGFloat($0)/CGFloat(size) * rect.size.height)
+            )
+        }
+    }
+    
+    
+    
+    func drawLine(start:CGPoint, end: CGPoint) {
+        let path = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        path.lineWidth = gridWidth
+        
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        path.move(to: start)
+        
+        //add a point to the path at the end of the stroke
+        path.addLine(to: end)
+        
+        //draw the stroke
+        gridColor.setStroke()
+        path.stroke()
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchedPosition = process(touches: touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchedPosition = process(touches: touches)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchedPosition = nil
+    }
+    
+    // Updated since class
+    typealias Position = (row: Int, col: Int)
+    var lastTouchedPosition: Position?
+    
+    func process(touches: Set<UITouch>) -> Position? {
+        guard touches.count == 1 else { return nil }
+        let pos = convert(touch: touches.first!)
+        guard lastTouchedPosition?.row != pos.row
+            || lastTouchedPosition?.col != pos.col
+            else { return pos }
+        
+        theGrid[pos] = theGrid[pos].toggle(theGrid[pos])
+       // print(theGrid[pos].description())
+        setNeedsDisplay()
+        return pos
+    }
+    
+    func convert(touch: UITouch) -> Position {
+        let touchY = touch.location(in: self).y
+        let gridHeight = frame.size.height
+        let row = touchY / gridHeight * CGFloat(size)
+        let touchX = touch.location(in: self).x
+        let gridWidth = frame.size.width
+        let col = touchX / gridWidth * CGFloat(size)
+        let position = (row: Int(row), col: Int(col))
+        return position
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
 
--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -5,14 +5,22 @@
 //  Created by Van Simmons on 1/15/17.
 //  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
 //
+//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stepButton: UIButton!
+    
+   
+    @IBOutlet weak var gridStoryboard: GridView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,47 +28,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBDesignable
-    class GridView: UIView {
-        var theGrid = Grid(0,0)
+    
+    @IBAction func stepButtonAction(_ sender: Any) {
+       gridStoryboard.theGrid = gridStoryboard.theGrid.next()
+       gridStoryboard.setNeedsDisplay()
         
         
-        required init?(coder aDecoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        @IBInspectable var size : Int = 20 {
-            didSet{
-                theGrid = Grid(size, size)
-            }
-        }
-        
-        @IBInspectable var livingColor : UIColor  = UIColor.blue
-        
-        @IBInspectable var emptyColor : UIColor = UIColor.white
-        
-        @IBInspectable var bornColor : UIColor = UIColor.green
-        
-        @IBInspectable var diedColor : UIColor = UIColor.black
-        
-        @IBInspectable var gridColor : UIColor = UIColor.gray
-        
-        @IBInspectable var gridWidth : CGFloat = 0.0
-        
-        init() {
-            super.init(frame: .zero)
-        }
         
     }
     
-    var oneGridView = GridView()
-    
-    oneGridView.livingColor = UIColor.green
-    oneGridView.bornColor = UIColor.green.withAlphaComponent(0.6)
-    
-    oneGridView.emptyColor = UIColor.darkGray
-    oneGridView.diedColor = UIColor.darkGray.withAlphaComponent(0.6)
-    oneGridView.gridColor = UIColor.black
-    oneGridView.gridWidth = 2.0
 }
 
